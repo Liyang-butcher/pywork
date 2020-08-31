57250d24d0e69fda57a5f38b7aa2a35a2ed00fa6
liyang <1612317953@qq.com>
2020-08-31 22:35:50 +0800

    第二次上课内容

--- lianxi.py
+++ lianxi.py
@@ -9,17 +9,24 @@
 
 # res = (1+10%3+10/5+1*3)
 # print(res)
-# """
+# 
 # a = input("请输入: ")
 # print("刚才输入的值: ",a)
 
-# """
+# res = (2**4)
+# print(res)
+# a = input()
+# print(type(a))
 # a = input("请输入: ")
 # b = input("请输入: ")
 # print("输出: ",float(a)+float(b))
 
-print(1>2)
+# print(1>2)
+# print(1<2)
 
+
+
+# print(type(None))
 
 # a = float(input("请输入: "))
 # b = float(input("请输入: "))
@@ -30,3 +37,31 @@ print(1>2)
 
 # print(len("sfdsfsfsg"))
 # print(len(12345))
+
+# a = (1,3,5,7,9,"哈哈",True,None,)
+# # print(a[6])
+# print(a.index(1))
+# print(a.count(1))
+# a = ((1,2),(3,4))
+# print(a[0][1])
+
+# a = ((1,2,(3,4)),(1,2))
+# print(a[0][2][1])
+# print(a[0][2].index(3))
+# print(a[0].count(1))
+
+# a = [1,3,5,7,9,"哈哈",True,None,]
+# name = input("请输入值: ")
+# a.append(name)
+# a.insert(4,name)
+# xx = a.pop(4)
+# print(xx)
+
+#字典
+a = {"name":"李杨","age":"18"}
+a["xinq"] = "喔嚯"
+print(a.get("xinq"))
+
+
+
+
